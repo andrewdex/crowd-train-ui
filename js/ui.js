a4ab30ce39fc55497db7acaf7e25d730fab95b81
Andrew dEX <andrewdexis@gmail.com>
2014-04-05 13:35:37 +0530

    Added New updates

---
+++ js/ui.js
@@ -0,0 +1,18 @@
+/*Crowd-Train Front-End Handler*/ 
+
+(function(Crowd_t, $, undefined){
+
+	crowd_t = {
+
+		Calert:function(msg){
+			alert(msg);
+		}
+
+	};
+
+
+})(window.crowd_t = window.crowd_t || {}, jQuery);
+
+
+// usage 
+//crowd_t.Calert("hey");
